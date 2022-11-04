9f859ee8ac30b920782b8fed6b104316aa3c7a22
raishabirah <raihanshabirah20@gmail.com>
2022-11-04 12:28:40 +0700

    added File

---
+++ tiketBioskop/Horor.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Horor here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Horor
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Horor
+     */
+    public Horor()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
